a712362fe39e9c85df07b3960b65697166017104
Cedric David <cedric.david@btopenworld.com>
2018-10-04 09:34:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;;
+  function addBump() {
+    counter++;
+  }
+  function getBumps() {
+    return counter;
+  }
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+}
+
+
+const sharkCreator = createAnimal("Shark");
+const sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+
+const sharkWithFrickinCannon = sharkCreator('Cannon');
