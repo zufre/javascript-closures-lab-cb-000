f8c434a891b09451740aba1abcfb1df12f07adc1
Cedric David <cedric.david@btopenworld.com>
2018-10-04 09:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,10 +13,17 @@ function bumpCounter() {
   };
 }
 
+// function createAnimal(animalType) {
+//   return function (deadlyDevise) {
+//     return { animalType, deadlyDevise };
+//   }
+// }
 function createAnimal(animalType) {
-  return function (deadlyDevise) {
-    return { animalType, deadlyDevise };
+ 
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
   }
+  
 }
 
 const sharkCreator = createAnimal("Shark");
